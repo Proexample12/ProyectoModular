65e036dfe55c633fe8de58d71a6242863a3002a3
Kenneth Zamora <kmzamora@uamv.edu.ni>
2025-09-22 14:42:23 -0600

    Agregar archivos de proyecto.

---
+++ CiudadesApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using CiudadesLibrary.Dao;
+using CiudadesLibrary.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CiudadesApp
+{
+    public partial class Form1 : Form
+    {
+        CiudadDao lista = new CiudadDao();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            
+            Ciudad ciudad = new Ciudad();
+
+            ciudad.Nombre = tbNombre.Text;
+            ciudad.poblacion = int.Parse(tbPoblacion.Text);
+            ciudad.FechaFundacion = dtpFundacion.Value;
+
+            lista.AgregarCiudad(ciudad);
+            llenardatagrid();
+        }
+        private void llenardatagrid()
+        {
+            dgvRegistros.DataSource = lista.MostrarCiudad();
+            dgvRegistros.Refresh();
+        }
+       
+    }
+}
---
+++ CiudadesLibrary/Dao/CiudadDao.cs
@@ -0,0 +1,33 @@
+﻿using CiudadesLibrary.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiudadesLibrary.Dao
+{
+    public class CiudadDao
+    {
+        private Ciudad[] listado = new Ciudad[10];
+        int pos = 0;
+
+        public void AgregarCiudad(Ciudad ciudad)
+        {
+            try
+            {
+                listado[pos] = ciudad;
+                pos++;
+            }catch(IndexOutOfRangeException)
+            {
+                Console.WriteLine("no puedes agregar mas de 10 ciudades");
+            }
+
+        }
+        public Ciudad[] MostrarCiudad()
+        {
+            return listado;
+        }
+        
+    }
+}
